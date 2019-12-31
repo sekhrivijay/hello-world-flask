c9835eb1aa1a62871b5cddb99de6fee59c4aedaa
Vijay Sekhri <vsekhri@ftdi.com>
2019-12-31 16:11:49 -0600

    Logging more infomration

--- server.py
+++ server.py
@@ -5,17 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	httpsEnabled = os.environ.get("HTTPS", False)
-        
-	return "Hello world!!!\n https " + httpEnabled
+        httpsEnabled = os.environ.get("HTTPS", False)
+        port = os.environ.get("PORT", 8080)
+        return "Hello world!!!\n https : " + httpsEnabled + "\n port : " + port + "\n" 
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 8080))
-	httpsEnabled = os.environ.get("HTTPS", False)
-	if httpsEnabled:
-		app.run(debug=True,host='0.0.0.0',port=port, ssl_context='adhoc')
-	app.run(debug=True,host='0.0.0.0',port=port)
+        port = int(os.environ.get("PORT", 8080))
+        httpsEnabled = os.environ.get("HTTPS", False)
+        if httpsEnabled:
+            app.run(debug=True,host='0.0.0.0',port=port, ssl_context='adhoc')
+        app.run(debug=True,host='0.0.0.0',port=port)
 	
-
-
-
