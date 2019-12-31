64a9c8bfeed629a29b252d362cd0b3844d2ca8e6
Vijay Sekhri <vsekhri@ftdi.com>
2019-12-31 16:33:32 -0600

    Version displayed

--- server.py
+++ server.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
         httpsEnabled = os.environ.get("HTTPS", False)
+        version = os.environ.get("VERSION", "v1")
         port = os.environ.get("PORT", 8080)
-        return "Hello world!!!\n https : " + httpsEnabled + "\n port : " + port + "\n" 
+        return "Hello world!!!\n https : " + httpsEnabled + "\n port : " + port + "\n version : " + version + "\n"
 
 if __name__ == "__main__":
         port = int(os.environ.get("PORT", 8080))
